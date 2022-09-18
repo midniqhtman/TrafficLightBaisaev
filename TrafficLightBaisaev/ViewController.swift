4ee0264c31a00614778f5884469d6198a91a8290
midniqhtman <c1esi@mail.ru>
2022-09-18 06:13:07 +0300

    Initial Commit

---
+++ TrafficLightBaisaev/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLightBaisaev
+//
+//  Created by Байсаев Зубайр on 18.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
